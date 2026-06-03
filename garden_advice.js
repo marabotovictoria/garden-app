26eb1fc907c9722aa306445c504c5b6819f77616
Victoria Maraboto <maraboto.victoria@gmail.com>
2026-06-02 19:23:38 -0700

    Added prompt() to the season and platType variables for user interaction (closes #1)

--- garden_advice.js
+++ garden_advice.js
@@ -1,6 +1,6 @@
 // Hardcoded values for the season and plant type
-let season = "summer"; // TODO: Replace with prompt() to allow user interaction.
-let plantType = "flower"; // TODO: Replace with prompt() to allow user interaction.
+let season = prompt("Enter the current season:"); // TODO: Replace with prompt() to allow user interaction.
+let plantType = prompt("Enter a plant type:"); // TODO: Replace with prompt() to allow user interaction.
 
 // Variable to hold gardening advice
 let advice = "";
